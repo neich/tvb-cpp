eb55bd7a7829318424e5646914e89b199734d827
Ignacio Martín <natx.mc@gmail.com>
2022-08-08 19:23:11 +0200

    changed run() signature, call to Model::init_dependant(), remove StateTrack references (now only monitors are used), removed commented code

--- src/simulator/simulator.cpp
+++ src/simulator/simulator.cpp
@@ -18,52 +18,41 @@
 
 using namespace tvb;
 
-StateTrack* Simulator::run(const Model *model,
-                           const Connectivity *connectivity,
-                           Integrator *integrator,
-                           Monitor* monitor,
-                           Coupling *coupling,
-                           double start_time, double end_time, double dt,
-                           State *initial_state,
-                           int samplingRate) {
+void Simulator::run(Model *model,
+                    const Connectivity *connectivity,
+                    Integrator *integrator,
+                    Monitor *monitor,
+                    Coupling *coupling,
+                    double start_time, double end_time, double dt,
+                    State *initial_state) {
 
     m_coupling = coupling;
 
     integrator->configure(start_time, end_time, dt);
+    model->init_dependant();
 
     auto n_steps = int((end_time - start_time) / dt);
 
     State state = initial_state != NULL ? *initial_state : model->initial();
-    StateTrack *result = model->create_track();
     double t = start_time;
 
-    // HistoryDense history(connectivity->weights(), connectivity->delays(), model->cvars());
     coupling->init(dt, state);
 
     auto n_reg = connectivity->weights().rows();
-    // result->push(state, t);
 
     TArray1d local_coupling = TArray1d::Zero(n_reg);
     TArray1d stimulus = TArray1d::Zero(n_reg);
 
     for (int step = 1; step <= n_steps; ++step) {
         TArray2d node_coupling = this->_loop_compute_node_coupling(step);
-//        if (tvb::isnan(node_coupling))
-//            std::cout << "Ein"; // TODO: debug! remove!
         this->_loop_update_stimulus(step, TArray1d()); // TODO: handle stimulus
         state = integrator->scheme(state, *model, node_coupling, local_coupling, stimulus);
-//        if (tvb::isnan(state))
-//            std::cout << "Ein"; // TODO: debug! remove!
+        if (tvb::isnan(state))
+            throw std::runtime_error("NaN found in integration state!");
         monitor->record(step, state);
         t += integrator->dt();
-//        if (step % samplingRate == 0)
-//            result->push(state, t);
         this->_loop_update_history(*coupling, step, state);
-        // TArray1d output = this->_loop_monitor_output(step, state)
-
     }
-
-    return result;
 }
 
 
