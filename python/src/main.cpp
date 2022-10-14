a3bb1e4ca833a290dc38725d00fe05617c5abca2
Ignacio Martín <natx.mc@gmail.com>
2022-10-14 17:15:13 +0200

    Added test method

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -1,11 +1,19 @@
 #include <pybind11/pybind11.h>
 
+#include <tvb-root-cpp/simulator/models/reduced_ww_ext.h>
+
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
 int add(int i, int j) {
     return i + j;
 }
+
+void model(int N, float value) {
+    auto *model = new tvb::ReducedWongWangExcInh(N);
+    model->set_param_fill("G", value);
+}
+
 
 namespace py = pybind11;
 
@@ -14,13 +22,14 @@ PYBIND11_MODULE(_core, m) {
         Pybind11 example plugin
         -----------------------
 
-        .. currentmodule:: tvb-root-cpp-python
+        .. currentmodule:: tvbcpp
 
         .. autosummary::
            :toctree: _generate
 
            add
            subtract
+           model
     )pbdoc";
 
     m.def("add", &add, R"pbdoc(
@@ -34,6 +43,14 @@ PYBIND11_MODULE(_core, m) {
 
         Some other explanation about the subtract function.
     )pbdoc");
+
+
+    m.def("model", &model, R"pbdoc(
+        Add two numbers
+
+        Some other explanation about the add function.
+    )pbdoc");
+
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
