85971cacd6e570f2a1317843c214a99682361694
Ignacio Martín <natx.mc@gmail.com>
2022-11-15 14:13:41 +0100

    Updated model

--- src/tvb-root-cpp/simulator/models/montbrio.cpp
+++ src/tvb-root-cpp/simulator/models/montbrio.cpp
@@ -27,22 +27,16 @@ State Montbrio::operator()(const State &x,
     const TArray1d &r_i = x.col(1);
     const TArray1d &u_e = x.col(2);
     const TArray1d &u_i = x.col(3);
-    const TArray1d &S_ee = x.col(4);
-    const TArray1d &S_ei = x.col(5);
-    const TArray1d &S_ie = x.col(6);
-    const TArray1d &S_ii = x.col(7);
+    const TArray1d &S_e = x.col(4);
     const TArray1d &c_0 = coupling.col(0);
 
-    derivative.col(0) = (delta_e / (M_PI*tau_e) + Float(2.0) * r_e * u_e) / tau_e; // no coupling
-    derivative.col(1) = (delta_i / (M_PI*tau_i) + Float(2.0) * r_i * u_i) / tau_i; // no coupling
+    I_e = I_e_ext + tau_e*S_e - J * J_G * r_i + J_A * G * c_0;
+    I_i = I_i_ext + tau_e*S_e - tau_i*J_G*r_i;
+    derivative.col(0) = (delta_e / (M_PI*tau_e) + Float(2.0) * r_e * u_e - g_e*r_e) / tau_e;
+    derivative.col(1) = (delta_i / (M_PI*tau_i) + Float(2.0) * r_i * u_i- g_i*r_i) / tau_i;
     derivative.col(2) = (eta_e + u_e.pow(2.0) - (r_e * M_PI * tau_e).pow(2.0) + I_e) / tau_e;
     derivative.col(3) = (eta_i + u_i.pow(2.0) - (r_i * M_PI * tau_i).pow(2.0) + I_i) / tau_i;
-    derivative.col(4) = -S_ee + J_ee*r_e;
-    derivative.col(5) = -S_ei + J_ei*r_i;
-    derivative.col(6) = -S_ie + J_ie*r_e;
-    derivative.col(7) = -S_ii + J_ii*r_i;
-    I_e = I_ext + tau_e*S_ee - J_e*S_ei + J_ee * G * c_0;
-    I_i = I_ext + tau_i*S_ie - tau_i*S_ii;
+    derivative.col(4) = (-S_e + J_N*r_e)/tau_N;
 
     return derivative;
 
--- src/tvb-root-cpp/simulator/models/montbrio.h
+++ src/tvb-root-cpp/simulator/models/montbrio.h
@@ -41,6 +41,8 @@ namespace tvb {
 //                doc = """[ms]. Inhibitory population decay time constant."""
 //        )
 
+        TArray1d tau_N;
+
         TArray1d G;
 //        = NArray(
 //                label = ":external:`G`",
@@ -54,93 +56,104 @@ namespace tvb {
         TArray1d delta_i;
         TArray1d eta_e;
         TArray1d eta_i;
+        TArray1d g_e;
+        TArray1d g_i;
+        TArray1d a_e;
+        TArray1d a_i;
         TArray1d I_e;
         TArray1d I_i;
-        TArray1d I_ext;
-        Float J_ee;
-        Float J_ei;
-        Float J_ie;
-        Float J_ii;
+        TArray1d I_e_ext;
+        TArray1d I_i_ext;
         TArray1d J_e;
         TArray1d J_i;
+        TArray1d J_A;
+        TArray1d J_G;
+        TArray1d J_N;
+        TArray1d J;
 
-        Float t_start;
-        Float t_end;
-        Float dt;
-        
     protected:
         int m_num_steps;
 
     public:
         Montbrio(int m_n_nodes) : Model(m_n_nodes) {
-            m_cvars = {4}; // coupling variable S_ee
-            m_state_vars = {"r_e", "r_i", "u_e", "u_i", "S_ee", "S_ei", "S_ie", "S_ii"};
+            m_cvars = {0}; // coupling variable r_e
+            m_state_vars = {"r_e", "r_i", "u_e", "u_i", "S_e"};
             m_n_vars = m_state_vars.size();
         }
         
         void set_param(const std::string& param, const TArray1d& value) override {
-            ADD_SETTER_VALUE(delta_e, delta_i, eta_e, eta_i, I_e, I_i, I_ext, J_e, J_i)
+            ADD_SETTER_VALUE(tau_e, tau_i, tau_N, G, delta_e, delta_i, eta_e, eta_i, a_e, a_i, g_e, g_i, I_e, I_i, I_e_ext, I_i_ext, J_e, J_i, J_A, J_G, J_N, J)
+            throw std::runtime_error(string_format("Parameter %s does not exist in this model", param.c_str()));
+
         }
 
         void set_param(const std::string& param, float value) override {
-            ADD_SETTER_FILL(delta_e, delta_i, eta_e, eta_i, I_e, I_i, I_ext, J_e, J_i)
-            ADD_SETTER_VALUE(t_start, t_end, dt)
+            ADD_SETTER_FILL(tau_e, tau_i, tau_N, G, delta_e, delta_i, eta_e, eta_i, a_e, a_i, g_e, g_i, I_e, I_i, I_e_ext, I_i_ext, J_e, J_i, J_A, J_G, J_N, J)
+            throw std::runtime_error(string_format("Parameter %s does not exist in this model", param.c_str()));
         }
 
         const TArray1d& get_param(const std::string& param) const override {
-            ADD_GETTER(delta_e, delta_i, eta_e, eta_i, I_e, I_i, I_ext, J_e, J_i)
+            ADD_GETTER(tau_e, tau_i, tau_N, G, delta_e, delta_i, eta_e, eta_i, a_e, a_i, g_e, g_i, I_e, I_i, I_e_ext, I_i_ext, J_e, J_i, J_A, J_G, J_N, J)
+            throw std::runtime_error(string_format("Parameter %s does not exist in this model", param.c_str()));
         }
 
-        void configure() {
+        void configure() override {
             tau_e.resize(m_n_nodes);
             tau_i.resize(m_n_nodes);
+            tau_N.resize(m_n_nodes);
             G.resize(m_n_nodes);
             delta_e.resize(m_n_nodes);
             delta_i.resize(m_n_nodes);
             eta_e.resize(m_n_nodes);
             eta_i.resize(m_n_nodes);
+            a_e.resize(m_n_nodes);
+            a_i.resize(m_n_nodes);
+            g_e.resize(m_n_nodes);
+            g_i.resize(m_n_nodes);
             I_e.resize(m_n_nodes);
             I_i.resize(m_n_nodes);
-            I_ext.resize(m_n_nodes);
+            I_e_ext.resize(m_n_nodes);
+            I_i_ext.resize(m_n_nodes);
             J_e.resize(m_n_nodes);
             J_i.resize(m_n_nodes);
-
+            J_A.resize(m_n_nodes);
+            J_G.resize(m_n_nodes);
+            J_N.resize(m_n_nodes);
+            J.resize(m_n_nodes);
 
             tau_e.fill(10.0);
             tau_i.fill(10.0);
-            J_i.fill(0.0);
+            tau_N.fill(10.0);
             G.fill(2.5);
             delta_e.fill(1.0);
             delta_i.fill(1.0);
             eta_e.fill(1.0);
             eta_i.fill(1.0);
+            a_e.fill(0.25);
+            a_i.fill(1.0);
+            g_e.fill(2.5);
+            g_i.fill(0);
             I_e.fill(0.0);
             I_i.fill(0.0);
-            I_ext.fill(0.0);
+            I_e_ext.fill(0.0);
+            I_i_ext.fill(0.0);
             J_e.fill(1.0);
             J_i.fill(0.0);
-            J_ee = 1.0;
-            J_ei = 0.0;
-            J_ie = 0.0;
-            J_ii = 0.0;
-            t_start = 0.0;
-            t_end = 10000.0;
-            dt = 0.1;
+            J_A.fill(1.0);
+            J_G.fill(1.0);
+            J_N.fill(1.0);
+            J.fill(10.0);
         }
 
         void init_dependant() override {
-            // TODO tau_av = floor(Float(1e-3) * tau / m_dt + Float(1e-6));
-//            m_utrace = TArray2d::Zero(m_n_nodes, m_num_steps+1);
-//            m_r = TArray2d::Zero(m_n_nodes, m_num_steps+1);
-            m_num_steps = floor((t_end - t_start) / dt);
-
+            J_N = J_e + g_e*a_e.log();
+            J_G = J_i + g_i*a_i.log();
         }
-
 
         [[nodiscard]] State initial() const override {
             State result(m_n_nodes, m_n_vars);
             TArray1d init_state(m_n_vars);
-            init_state << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+            init_state << 0.0, 0.0, 0.0, 0.0, 0.0;
             for (int i = 0; i < m_n_nodes; ++i)
                 result.row(i) = init_state;
             return result;
@@ -149,7 +162,7 @@ namespace tvb {
         void initial(State &state) const override {
             state.resize(m_n_nodes, m_n_vars);
             TArray1d init_state(m_n_vars);
-            init_state << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+            init_state << 0.0, 0.0, 0.0, 0.0, 0.0;
             for (int i = 0; i < m_n_nodes; ++i)
                 state.row(i) = init_state;
         }
