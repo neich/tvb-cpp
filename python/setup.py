e6c03bf3913569eecce8d4c39dc17b998a047300
Ignacio Martín <natx.mc@gmail.com>
2023-03-03 18:18:44 +0100

    New file

--- python/setup.py
+++ python/setup.py
@@ -15,13 +15,9 @@ from setuptools import find_packages
 setup(
     name="tvbcpp",
     version="0.0.3",
-    description="a minimal example package (with pybind11)",
-    author="Ignacio Martín",
-    license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     cmake_install_dir="src/tvbcpp",
     include_package_data=True,
-    extras_require={"test": ["pytest"]},
     python_requires=">=3.6",
 )
