2f388553f72bb2b44fba282af9fcff5cbf4b02c3
Ignacio Martin <natx.mc@gmail.com>
2023-08-11 19:36:23 +0200

    Bump version to 0.0.4

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ from setuptools import find_packages
 
 setup(
     name="tvbcpp",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     cmake_install_dir="src/tvbcpp",
