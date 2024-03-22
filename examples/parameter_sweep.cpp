b34f40de4add58d21aaa722439325c1acbdf339c
Ignacio Martin <natx.mc@gmail.com>
2024-03-22 14:42:21 +0100

    Refactored to use new CLParser class

--- examples/parameter_sweep.cpp
+++ examples/parameter_sweep.cpp
@@ -16,283 +16,132 @@
 #include <chrono>
 
 #include <tvb-cpp/tools/npz_tools.h>
+#include <tvb-cpp/simulator/factory.h>
 #include <tvb-cpp/simulator/simulate.h>
 #include <tvb-cpp/simulator/monitor.h>
 #include <tvb-cpp/simulator/models/reduced_ww_ext.h>
 #include <tvb-cpp/simulator/models/montbrio.h>
 #include <tvb-cpp/simulator/integrators/euler_stochastic.h>
-#include <tvb-cpp/tools/threadpool.h>
 #include <tvb-cpp/tools/csv_tools.h>
 #include <tvb-cpp/simulator/integrators/euler_deterministic.h>
 #include <tvb-cpp/simulator/models/zerlaut.h>
 #include <tvb-cpp/simulator/simulator.h>
 #include <tvb-cpp/simulator/bold/bold_tvb.h>
-#include <tvb-cpp/simulator/bold/bold_BalloonWindkessel.h>
+#include "tvb-cpp/simulator/bold/bold_BalloonWindkessel.h"
+#include "tvb-cpp/simulator/bold/bold_Stephan2007.h"
 
-// #include <tvb-cpp/matplotlibcpp.h>
 #include <chrono>
-#include <filesystem>
-#include <thread>
-
 #include <boost/program_options.hpp>
 #include <utility>
+#include <filesystem>
+#include <tvb-cpp/tools/cl_params.h>
 
-using namespace boost::program_options;
+
 using namespace tvb;
 using namespace std::chrono;
-// amespace plt = matplotlibcpp;
+using namespace boost::program_options;
 
-struct SweepParam {
-    std::string name;
-    std::vector<float> values;
-};
-
-struct Parameter {
-    std::string name;
-    float value;
-
-    Parameter(std::string name, float value) : name(std::move(name)), value(value) {}
-};
-
-struct RunParams {
-    float dt = 0.1;
-    float t_start = 0.0;
-    float t_end = 10000.0;
-    bool force_output = false;
-    tvb::Monitor *monitor = nullptr;
-    std::vector<Parameter> params;
-    std::string file_out;
-    std::string file_prefix;
-    std::string file_weights;
-    std::string file_lengths;
-    float speed = 1e6;
-
-    RunParams() = default;
-
-    RunParams(std::vector<Parameter> params, tvb::Monitor *monitor) : params(std::move(params)), monitor(monitor) {}
-};
+int main(int argc, const char ** argv) {
 
 
-void save_fig(RunParams &rp);
-
-RunParams run(RunParams rp) {
-
-    std::string filename = rp.file_prefix;
-    for (auto const &p: rp.params) {
-        filename += string_format("_%s_%.2f", p.name.c_str(), p.value);
-    }
-    filename += ".png";
-
-    if (!rp.force_output && std::filesystem::exists(filename)) {
-        std::cout << string_format("File %s already exists", filename.c_str()) << std::endl;
-        delete rp.monitor;
-        rp.monitor = nullptr;
-        return rp;
-    }
-
-    tvb::TArray2d C;
-    if (rp.file_weights.ends_with(".csv"))
-        C = tvb::csv_load(rp.file_weights);
-    else if (rp.file_weights.ends_with(".npz"))
-        C = tvb::npz2Matrixd(rp.file_weights, "SC");
-    else
-        throw std::runtime_error(string_format("Unknown file extension for: %s", rp.file_weights.c_str()));
-
-    int N = C.rows();
-
-    // C = C / C.rowwise().sum().maxCoeff() * 2.0;
-    // tvb::csv_save("sc_d_norm.csv", C);
-
-    tvb::TArray2d tl;
-    if (!rp.file_lengths.empty())
-        tl = tvb::csv_load(rp.file_lengths);
-    else
-        tl = tvb::TArray2d::Zero(C.rows(), C.cols());
-    tvb::Connectivity con(C, tl, rp.speed);
-
-    milliseconds total_time(0);
-    std::cout << string_format("Starting computation for: %s", filename.c_str()) << std::endl;
-
-    auto *model = new tvb::Montbrio(N);
-    model->set_param("t_start", rp.t_start);
-    model->set_param("t_end", rp.t_end);
-    model->set_param("dt", rp.dt);
-    model->init_dependant();
-    // auto *model = new tvb::ReducedWongWangExcInh(N);
-    // auto *model = new tvb::ZerlautAdptationSecondOrder(N);
-    for (auto const &p: rp.params)
-        model->set_param(p.name, p.value);
-
-    // rp.monitor = new tvb::BoldTVB(N, 720.0, rp.dt, {0});
-    // rp.monitor = new tvb::BoldBalloonWindkessel(N, 1.0, 720.0, rp.dt, {0});
-    rp.monitor = new tvb::RawSubSample(1.0, rp.dt, {0});
-
-    // auto *model = new tvb:std:ZerlautAdaptationFirstOrder(N);
-    // tvb::TArray1d sigmas(4);
-    // sigmas << 3e-5, 3e-5, 0.0, 0.0;
-    // auto *integrator = new tvb::EulerStochastic(new Additive(sigmas, 0.1));
-    auto *integrator = new tvb::EulerDeterministic(rp.dt);
-    auto coupling = new tvb::CouplingLinearSparse(con.weights(), con.delays(), model->cvars());
-
-
-    auto start = std::chrono::high_resolution_clock::now();
-    tvb::Simulator simulator{};
-    simulator.run(model,
-                  &con,
-                  integrator,
-                  {rp.monitor},
-                  coupling,
-                  rp.t_start, rp.t_end,
-                  nullptr);
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::high_resolution_clock::now() - start);
-
-    std::cout << string_format("Simulation time (%s): %d msecs", filename.c_str(), duration.count()) << std::endl;
-
-    total_time += duration;
-
-    delete model;
-    delete coupling;
-
-    rp.file_out = filename;
-    return rp;
-}
-
-
-void save_fig(RunParams &rp) {
-    if (rp.monitor == nullptr) return;
-
-    int n_records = rp.monitor->getRecords().size();
-    int N = rp.monitor->getRecords()[0].record.rows();
-    std::vector<std::vector<Float>> y_plot(N, std::vector<Float>(n_records));
-    for (unsigned t = 0; t < n_records; ++t) {
-        y_plot.emplace_back();
-        const Monitor::Record &r = rp.monitor->getRecords()[t];
-        for (unsigned n = 0; n < N; ++n)
-            y_plot[n][t] = r.record(n, 0);
-    }
-
-    // tvb::csv_save("./test_simulationRWW_TVB_CPP.csv", y_plot);
-
-    // Plot line from given x and y data. Color is selected automatically.
-    std::vector<Float> ls(n_records);
-    std::transform(rp.monitor->getRecords().begin(), rp.monitor->getRecords().end(), ls.begin(),
-                   [](const Monitor::Record &r) { return r.time/1000; });
-//    for (unsigned n = 0; n < N; ++n) {
-//        plt::plot(ls, y_plot[n]);
-//    }
-    // Plot a red dashed line from given x and y data.
-    // plt::plot(x, w,"r--");
-    // Plot a line whose name will show up as "log(x)" in the legend.
-
-    std::string title = "Reduced Wong Wang: ";
-    for (auto const &p: rp.params) {
-        title += string_format(" %s_%.2f", p.name.c_str(), p.value);
-    }
-
-//    plt::title(title);
-//    plt::ylabel("Se");
-//    plt::xlabel("Seconds");
-//    // Save the image (file format is determined by the extension)
-//    plt::save(rp.file_out, 300);
-//
-//    plt::clf();
-
-    delete rp.monitor;
-}
-
-void to_cout(const std::vector<std::string> &v) {
-    std::copy(v.begin(), v.end(), std::ostream_iterator<std::string>{
-            std::cout, "\n"});
-}
-
-int main(int argc, char **argv) {
-    try {
-        options_description desc{"Options"};
-        desc.add_options()
-                ("help,h", "Help screen")
-                ("params", value<std::vector<std::string>>()->multitoken()->required(), "Parameters to sweep")
-                ("sc-matrix", value<std::string>()->required(), "Structural connectivity matrix")
-                ("length-matrix", value<std::string>(), "Connection lengths matrix matrix")
-                ("speed", value<float>()->default_value(1e6), "Signal speed")
-                ("time-start", value<float>()->default_value(0.0), "Start of simulation (ms)")
-                ("time-end", value<float>()->default_value(10000.0), "End of simulation (ms)")
-                ("dt", value<float>()->default_value(0.1), "Integration step (ms)")
-                ("force-output", bool_switch()->default_value(false), "Force overwrite output files")
-                ("out-file-prefix", value<std::string>()->required(), "Output file prefix");
-
-        variables_map vm;
-        store(parse_command_line(argc, argv, desc), vm);
-        notify(vm);
-
-        std::vector<SweepParam> params;
-        if (vm.count("help"))
-            std::cout << desc << '\n';
-        else if (vm.count("params")) {
-            for (auto &s: vm["params"].as<std::vector<std::string>>()) {
-                if (std::isalpha(s[0])) {
-                    if (!params.empty() && !(params.back().values.size() == 1 || params.back().values.size() == 3))
-                        throw std::runtime_error(string_format("Malformed parameter <%s>\n", s.c_str()));
-                    params.emplace_back();
-                    params.back().name = s;
-                } else {
-                    try {
-                        float value = std::stof(s);
-                        params.back().values.push_back(value);
-                    } catch (const error &ex) {
-                        throw std::runtime_error("Syntax error in sweep parameters");
-                    }
-                }
-            }
-            if (params.back().values.size() != 1 && params.back().values.size() != 3)
-                throw std::runtime_error(string_format("Malformed parameter <%s>\n", params.back().name.c_str()));
+/*
+    auto *b = new BoldTVB(2000.0);
+    int t_length = 1000000;
+    int n_rois = 76;
+    tvb::TArray2d tmp = tvb::TArray2d::Zero(t_length, n_rois);
+    int val = 0;
+    for (int t = 0; t < t_length; ++t)
+        for (int n = 0; n < n_rois; ++n) {
+            tmp(t, n) = val % 13;
+            val++;
         }
 
-        std::vector<RunParams> param_combs(1);
-        for (auto const &p: params) {
-            if (p.values.size() == 1) {
-                for (auto &pc: param_combs)
-                    pc.params.emplace_back(p.name, p.values[0]);
-            } else {
-                std::vector<RunParams> new_param_combs;
-                for (auto v: tvb::range(p.values[0], p.values[1], p.values[2])) {
-                    for (auto const &pc: param_combs) {
-                        new_param_combs.push_back(pc);
-                        new_param_combs.back().params.emplace_back(p.name, v);
-                    }
-                }
-                param_combs = new_param_combs;
-            }
-        }
+    tmp = tvb::npy2Matrixd("test_CNT.npy");
+    auto [t_tmp, data_tmp] = b->compute_bold(tmp, 1);
 
-        tvb::ThreadPool<RunParams> tp(6);
-        tp.start();
-        for (auto &pc: param_combs) {
-            pc.file_weights = vm["sc-matrix"].as<std::string>();
-            pc.t_start = vm["time-start"].as<float>();
-            pc.t_end = vm["time-end"].as<float>();
-            pc.dt = vm["dt"].as<float>();
-            if (vm.count("length-matrix"))
-                pc.file_lengths = vm["length-matrix"].as<std::string>();
-            pc.file_prefix = vm["out-file-prefix"].as<std::string>();
-            pc.speed = vm["speed"].as<float>();
-            pc.force_output = vm.count("force-output") > 0;
-            tp.queue_job([pc] { return run(pc); });
-        }
+    tvb::Matrixd2np(data_tmp, "tmp_bold_tvb_cpp.npy");
+*/
 
-        while (!tp.finished()) {
-            std::optional<RunParams> op = tp.get_result();
-            if (op.has_value())
-                save_fig(op.value());
-            sleep(1);
-        }
+    std::vector<std::pair<std::string, Float>> params;
 
-        tp.stop();
+    options_description desc{"Options"};
+    desc.add_options()
+            ("speed", value<tvb::Float>()->default_value(1e6), "Signal speed")
+            ("time-start", value<tvb::Float>()->default_value(0.0), "Start of simulation (ms)")
+            ("time-end", value<tvb::Float>()->default_value(10000.0), "End of simulation (ms)")
+            ("dt", value<tvb::Float>()->default_value(0.1), "Integration step (ms)")
+            ("params-file", value<std::string>(), "NPZ file with simulation parameters")
+            ("out-file-prefix", value<std::string>()->default_value("out_sim"), "Output file prefix");
 
+    tvb::CLParser cp;
+    cp.init(desc, argc, argv);
+
+    std::string out_prefix = cp.get_option<std::string>("out-file-prefix");
+
+    int N = cp.get_n_rois();
+    std::cout << string_format("Connectivity matrix size: %i", N) << std::endl;
+
+    tvb::Connectivity con(*cp.get_sc_matrix(), *cp.get_length_matrix(), cp.get_option<tvb::Float>("speed"));
+
+    auto *model = tvb::Factory::new_model(cp.get_option<std::string>("model"), N);
+    auto dt = cp.get_option<tvb::Float>("dt");
+    tvb::TArray1d sigmas = cp.get_noise();
+    auto *integrator = new tvb::EulerStochastic(dt, new Additive(sigmas, dt));
+    int voi = 0;
+    tvb::Float time_end = cp.get_option<tvb::Float>("time-end");
+
+    if (model->n_vars() != sigmas.size())
+        throw std::runtime_error(string_format("Provided noise size (%i) does not mathc with model number of state variables (%i)", sigmas.size(), model->n_vars()));
+
+    auto param_combinations = cp.get_parameter_combinations();
+    int n_sim = 0;
+    for (const tvb::ParameterSet& pset: param_combinations) {
+
+        tvb::Float G = cp.init_from_parameters(model, pset);
+
+        // auto *integrator = new tvb::EulerDeterministic(dt);
+        auto *coupling = new tvb::CouplingLinearSparse(con.weights(), con.delays(), model->cvars());
+        coupling->setScale(G);
+
+        std::string file_prefix = string_format("%s_%d", out_prefix.c_str(), n_sim);
+        std::cout << string_format("Starting computation for: %s", out_prefix.c_str()) << std::endl;
+        auto start = std::chrono::high_resolution_clock::now();
+
+        Simulator simulator{};
+        TArray2d initial_state = TArray2d::Zero(cp.get_n_rois(), model->n_vars());
+        auto *monitor = new TemporalAverage(con.weights().cols(), 1.0, dt, {0});
+
+        simulator.run(model,
+                      &con,
+                      integrator,
+                      {monitor},
+                      coupling,
+                      0, time_end,
+                      nullptr,
+                      &initial_state);
+
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+        std::cout << string_format("Simulation time: %d msecs", duration.count()) << std::endl;
+
+        TArray2d voi_0 = monitor->voi2Array(voi);
+        size_t n_records = monitor->getRecords().size();
+
+        tvb::TArray2dMap map_raw;
+        map_raw["t_samples"] = tvb::nrange(0.0, 1.0, n_records);
+        map_raw["data"] = voi_0;
+        tvb::MatrixdMap2npz(file_prefix + "_RAW.npz", map_raw);
+
+        auto *bold_model_tvb = new BoldTVB(2000.0);
+        tvb::TArray2dMap map_bold_tvb;
+        auto [t_samples_bold_tvb, data_tvb] = bold_model_tvb->compute_bold(voi_0, 1.0);
+        map_bold_tvb["t_samples"] = t_samples_bold_tvb;
+        map_bold_tvb["data"] = data_tvb;
+        tvb::MatrixdMap2npz(file_prefix + "_BOLD.npz", map_bold_tvb);
+
+        n_sim++;
     }
-    catch (const error &ex) {
-        std::cerr << ex.what() << '\n';
-    }
+
+    return 0;
 }
